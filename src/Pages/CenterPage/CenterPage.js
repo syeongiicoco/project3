6828ce834820e1f00e33dcf7150e5ad3a5a1d8b6
syeongiicoco <syeongii78@gmail.com>
2021-05-27 15:08:18 +0900

    저장

--- src/Pages/CenterPage/CenterPage.js
+++ src/Pages/CenterPage/CenterPage.js
@@ -6,18 +6,28 @@ class CenterPage extends Component {
   state = {
     num: 0,
     banner: 0,
+    x: 0,
+    centerPageData: [],
   };
 
   componentDidMount() {
-    setInterval(() => {
-      const { num } = this.state;
-      this.setState({ num: num + 1 > 3 ? 0 : num + 1 });
-    }, 3000);
+    // setInterval(() => {
+    //   const { num } = this.state;
+    //   this.setState({ num: num + 1 > 3 ? 0 : num + 1 });
+    // }, 3000);
 
-    setInterval(() => {
-      const { banner } = this.state;
-      this.setState({ banner: banner + 1 > 1 ? 0 : banner + 1 });
-    }, 2000);
+    // setInterval(() => {
+    //   const { banner } = this.state;
+    //   this.setState({ banner: banner + 1 > 1 ? 0 : banner + 1 });
+    // }, 2000);
+
+    fetch('http://localhost:3000/data/TodayHotDeal.json')
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          centerPageData: response,
+        });
+      });
   }
 
   goToText = e => {
@@ -25,7 +35,24 @@ class CenterPage extends Component {
     this.props.history.push(`/center/${e}`);
   };
 
+  nextButton = () => {
+    const { x } = this.state;
+
+    this.setState({ x: x - 300 }, () => {
+      console.log(this.state.x);
+    });
+  };
+
+  beforeButton = () => {
+    const { x } = this.state;
+
+    this.setState({ x: x + 300 }, () => {
+      console.log(this.state.x);
+    });
+  };
+
   render() {
+    console.log(this.nextButton);
     const { num, banner } = this.state;
 
     return (
@@ -62,6 +89,23 @@ class CenterPage extends Component {
           />
           <div className="todayHotDeal">
             <div className="todayHotDealText">오늘의 핫딜!</div>
+            <div className="todayHotDealInfo">
+              <div className="todayHotDealImgBox">
+                {this.state.centerPageData.map((centerData, key) => {
+                  return (
+                    <div
+                      className="todayHotDealImg"
+                      style={{ backgroundImage: `url('${centerData.img}')` }}
+                    />
+                  );
+                })}
+              </div>
+
+              <div className="arrowBox">
+                <i class="fas fa-arrow-left" onClick={this.beforeButton} />
+                <i class="fas fa-arrow-right" onClick={this.nextButton} />
+              </div>
+            </div>
           </div>
         </div>
       </section>
